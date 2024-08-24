d4fb48261fc61236052c34831e6e7c6ab329183a
Md. Tanvir Hossain <151984579+tanvirhossain808@users.noreply.github.com>
2024-08-25 02:51:55 +0600

    fix: fix some type related error

--- src/components/VoiceSearch.tsx
+++ src/components/VoiceSearch.tsx
@@ -2,10 +2,11 @@
 
 import useVoiceApi from "@/hooks/useVoiceApi";
 const VoiceSearch: React.FC = () => {
-    const { startVoiceRecognition } = useVoiceApi()
+    // const { startVoiceRecognition } = useVoiceApi()
 
     return (
-        <button onClick={startVoiceRecognition} type="button" className="block h-[30px] w-[30px] rounded-full bg-gray-600 ml-5">🎤</button>
+        // <button onClick={startVoiceRecognition} type="button" className="block h-[30px] w-[30px] rounded-full bg-gray-600 ml-5">🎤</button>
+        ""
 
     );
 };
